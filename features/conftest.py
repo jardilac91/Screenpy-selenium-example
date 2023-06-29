0198054263ba08157c952bffa92dfab272dd2569
Jorge_Ardila <jardilac91@gmail.com>
2023-06-29 17:05:36 -0500

    se modifica ejemplo a realizar búsqueda en github

--- features/conftest.py
+++ features/conftest.py
@@ -1,4 +1,5 @@
 from typing import Generator
+from webbrowser import Chrome
 
 from screenpy import Actor
 from screenpy.pacing import the_narrator
@@ -7,19 +8,16 @@ from screenpy_selenium.abilities import BrowseTheWeb
 
 from screenpy_adapter_allure import AllureAdapter
 
-from configuration.driver_manager import CustomDriver
-
 import pytest
 
 
 @pytest.fixture(scope="function", name="Usuario")
 def fixture_actor() -> Generator:
     """Inicializamos el usuario y le entregamos la habilidad navegar en internet utilizando chrome"""
-    actor = Actor.named("Usuario").who_can(BrowseTheWeb.using(CustomDriver.chrome()))
+    actor = Actor.named("Usuario").who_can(BrowseTheWeb.using_chrome())
     yield actor
     actor.exit()
 
 """Agregamos el adaptador de allure para generar los reportes"""
 the_narrator.adapters.append(AllureAdapter())
 
-URL = "https://google.com.co"
--- locators/ResultPage.py
+++ locators/ResultPage.py
@@ -2,11 +2,10 @@ from screenpy_selenium import Target
 from selenium.webdriver.common.by import By
 
 
-result = Target.the("result").located_by(
-    (By.XPATH, '/html/body/div[7]/div/div[13]/div/div[2]/div[2]/div/div/div[2]/div/div/div[1]/div/a/h3')
+results = Target.the("results").located_by(
+    (By.CSS_SELECTOR, 'div.codesearch-results > div > div > h3')
 )
 
-result_section = Target.the("result_section").located_by(
-    (By.ID, "rso")
-)
+
+
 
