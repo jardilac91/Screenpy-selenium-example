bfdf44362e264d4a20d4b2830f7660f358ca6853
Jorge_Ardila <jardilac91@gmail.com>
2023-06-29 18:10:01 -0500

    se agrega archivo para el uso de chromedriver con el argumento headless

--- features/conftest.py
+++ features/conftest.py
@@ -8,13 +8,15 @@ from screenpy_selenium.abilities import BrowseTheWeb
 
 from screenpy_adapter_allure import AllureAdapter
 
+from configuration.driver_manager import CustomDriver
+
 import pytest
 
 
 @pytest.fixture(scope="function", name="Usuario")
 def fixture_actor() -> Generator:
     """Inicializamos el usuario y le entregamos la habilidad navegar en internet utilizando chrome"""
-    actor = Actor.named("Usuario").who_can(BrowseTheWeb.using_chrome())
+    actor = Actor.named("Usuario").who_can(BrowseTheWeb.using(CustomDriver.chrome()))
     yield actor
     actor.exit()
 
