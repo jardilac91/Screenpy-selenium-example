13b9d21faa35929770f5834e3559b658dedda3c9
Jorge_Ardila <jardilac91@gmail.com>
2023-06-28 19:38:07 -0500

    se agrega full xpath para result

--- locators/ResultPage.py
+++ locators/ResultPage.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.by import By
 
 
 result = Target.the("result").located_by(
-    (By.XPATH, '//*[@id="rso"]/div[2]/div/div/div[1]/div/a/h3')
+    (By.XPATH, '/html/body/div[7]/div/div[13]/div/div[2]/div[2]/div/div/div[2]/div/div/div[1]/div/a/h3')
 )
 
 result_section = Target.the("result_section").located_by(
